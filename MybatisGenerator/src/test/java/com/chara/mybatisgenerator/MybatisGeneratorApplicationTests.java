1fc636a652f4954245c7dc1de846f419cd77c187
chara <2768946942@qq.com>
2024-02-24 14:07:57 +0800

    Mybatis-plus generator

---
+++ MybatisGenerator/src/test/java/com/chara/mybatisgenerator/MybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chara.mybatisgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
